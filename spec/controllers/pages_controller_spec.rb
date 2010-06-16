6fde31d79e147885caa9a92310b867057278e9e7
Jake Teton-Landis <jake.teton.landis@berkeley.edu>
2010-06-16 11:18:46 -0700

    Mapped routs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -38,5 +38,17 @@ describe PagesController do
       response.should have_tag("title", "Recipes on Rails | About")
     end
   end 
+   
+  describe "GET 'help'" do
+    it "should be sucessful" do
+      get 'help'
+      response.should be_success
+    end
     
+    it "shpuld have the right title" do
+      get 'help' 
+      response.should have_tag("title", "Recipes on Rails | Help")
+    end
+  end 
+ 
 end

fe05a0aaf81a4085876de7412608af6b5b935fa0
Jake Teton-Landis <jake.teton.landis@berkeley.edu>
2010-06-22 01:46:31 -0700

    modularized navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,33 @@ module ApplicationHelper
     end
   end
   
+  def crumb( args = {} )
+    opts = {:ul => "breadcrumb"}.merge( args )
+    
+    start = "<ul class=\"#{opts[:ul]} \" >
+      <li class=\"#{opts[:li]}\"><a class=\"#{opts[:a]}\"href=\"/\">Home</a></li>
+      "
+
+    s = ""
+    url = request.path.split('?')  #remove extra query string parameters
+    levels = url[0].split('/') #break up url into different levels
+    levels.each_with_index do |level, index|
+      unless level.blank?
+        if index == levels.size-1 || 
+           (level == levels[levels.size-2] && levels[levels.size-1].to_i > 0)
+          s += "<li class=\"#{opts[:li]}\">#{level.gsub(/_/, ' ')}</li>\n" unless level.to_i > 0
+        else
+            link = "/"
+            i = 1
+            while i <= index
+              link += "#{levels[i]}/"
+              i+=1
+            end
+            s += "<li class=\"#{opts[:li]}\"><a class=\"#{opts[:a]}\" href=\"#{link}\">#{level.gsub(/_/, ' ')}</a></li>\n"
+        end
+      end
+    end
+    fin = "</ul>"
+    start + s + fin
+  end
 end
